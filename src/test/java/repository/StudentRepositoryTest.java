6c169626c6756e06ae28af2aa628d0e310f6decc
AdrianButnar <butnar.adrian@gmail.com>
2019-03-25 16:44:13 +0200

    Merge?
    Merge!!

---
+++ src/test/java/repository/StudentRepositoryTest.java
@@ -0,0 +1,12 @@
+package repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentRepositoryTest {
+    @Test
+    public void test(){
+        assert(true);
+    }
+}
